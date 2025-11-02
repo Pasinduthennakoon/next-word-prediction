55356fd44202c3d843b8bc898810b59657747dd4
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-03 02:37:30 +0530

    create predict

--- predict.py
+++ predict.py
@@ -12,7 +12,30 @@ with open("tokenizer.pkl", "rb") as f:
 # Input text
 input_text = "cricket begin"
 
-# Convert to sequence using the trained tokenizer
-token_list = tokenizer.texts_to_sequences([input_text])[0]
+# Define the maximum length (same as during training)
+max_len = model.input_shape[1] + 1  # you can also hardcode 55 if you know it
 
-print("Tokenized input:", token_list)
+# Generate 6 words
+for i in range(6):
+    token_list = tokenizer.texts_to_sequences([input_text])[0]
+    token_list = tf.keras.preprocessing.sequence.pad_sequences([token_list], maxlen=max_len - 1, padding='pre')
+
+    # Predict next word
+    predicted_probs = model.predict(token_list, verbose=0)
+    predicted_id = np.argmax(predicted_probs, axis=-1)[0]  # get integer id
+
+    # Find the corresponding word
+    output_word = ""
+    for word, index in tokenizer.word_index.items():
+        if index == predicted_id:
+            output_word = word
+            break
+
+    # Stop if no word found
+    if output_word == "":
+        break
+
+    # Append word to input
+    input_text += " " + output_word
+
+print("Generated text:", input_text)
