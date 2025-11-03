3c1501b70fd6b0dbc3d7ede122dbc10e41482b3c
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-04 00:30:39 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 import numpy as np
 import pickle
 
+input_text = input("enter your input text:")
+cnt = int(input("enter your word count:"))
+
 # Load model
 model = tf.keras.models.load_model('model.h5')
 
@@ -10,13 +13,13 @@ with open("tokenizer.pkl", "rb") as f:
     tokenizer = pickle.load(f)
 
 # Input text
-input_text = "cricket begin"
+# input_text = "cricket begin"
 
 # Define the maximum length (same as during training)
 max_len = model.input_shape[1] + 1  # you can also hardcode 55 if you know it
 
 # Generate 6 words
-for i in range(6):
+for i in range(cnt):
     token_list = tokenizer.texts_to_sequences([input_text])[0]
     token_list = tf.keras.preprocessing.sequence.pad_sequences([token_list], maxlen=max_len - 1, padding='pre')
 
